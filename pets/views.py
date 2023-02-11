ca3167c7addba490863f7e54f662479f69b8bc9d
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-11 15:59:36 -0500

    index and create routes

--- pets/views.py
+++ pets/views.py
@@ -4,6 +4,22 @@ import json
 from .models import Turtle
 from django.views import View
 from .helper import GetBody
+from django.core.serializers import serialize
 
 # Create your views here.
+
+class TurtleView(View):
+    ## Index
+    def get(self, request):
+        all = Turtle.objects.all() # get all the turtle objects
+        serialized = serialize("json", all) # turn objects into json strings
+        finalData = json.loads(serialized) # turn json strings into dictionaries
+        return JsonResponse(finalData, safe=False)
+    
+    ## Create
+    def post(self, request):
+        body = GetBody(request)
+        turtle = Turtle.objects.create(name=body["name"], age=body["age"])
+        finalData = json.loads(serialize("json", [turtle]))
+        return JsonResponse(finalData, safe=False)
 
--- seconddjango/urls.py
+++ seconddjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pets.views import TurtleView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('turtle/', TurtleView.as_view())
 ]
